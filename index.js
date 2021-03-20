8e561b686a1952f918375275ce6b4dd1ce39f1d3
etamar <etamar.levy@gmail.com>
2021-03-19 20:58:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function agenda(shouldDo = 'go to the office') {
+    return `This Monday, I will ${shouldDo}.`
+}
+
+
+function wrapAdjective(marking = "*") {
+    return function(something = "special") {
+        return `You are ${marking}${something}${marking}!`
+    }   
+}
